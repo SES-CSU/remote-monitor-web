e01c0898bd19673b1512258da3d9fff79f7c2741
tarrantja <tarrantja@gmail.com>
2020-10-18 22:11:14 +0800

    License Info

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+# This work is licensed under the Creative Commons Attribution-NonCommercial 3.0 Australia License.  
+# Created by: Rio Akbar, Calvin Nguyen, Erik Stefan, James Tarrant, Dylan Turner
+
 from flask import Flask
 from config import Config
 from flask_bootstrap import Bootstrap
